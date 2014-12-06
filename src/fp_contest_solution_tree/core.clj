cc2d6b0f7daee4906c3e846c61c6ff004a5ddabc
alexpetrov <alexpetrov.rb@gmail.com>
2014-12-07 00:08:02 +0300

    First implementation of `solve-forest` with recursion

--- src/fp_contest_solution_tree/core.clj
+++ src/fp_contest_solution_tree/core.clj
@@ -1,35 +1,51 @@
 (ns fp-contest-solution-tree.core
   (:require [mc.util :refer :all]))
 
-(defn alternatives [feature-values]
-  (reduce-indexed (fn [acc idx itm]
-                    (assoc acc itm (conj (get acc itm #{}) idx))) {} feature-values))
+(def undefined "-")
 
-(defn level-zero-forest
-  [[feature-name feature-values]]
-  (let [alternatives (alternatives feature-values)]
-    (into #{}
-        (for [answer (keys alternatives)]
-          {:feature feature-name :answer answer :alternatives (get alternatives answer)}))))
+(defn alternatives
+  ([feature-values]
+     (alternatives feature-values nil))
+  ([feature-values indexes]
+     (reduce-indexed
+      (fn [acc idx itm]
+        (if (and (or (nil? indexes) (indexes idx))
+                 (not= undefined itm))
+          (assoc acc itm (conj (get acc itm #{}) idx))
+          acc)) {} feature-values)))
 
+(defn solve-forest
+  ([features] (solve-forest features nil))
+  ([[[feature-name feature-values] & more] constraints]
+     (let [alternatives (alternatives feature-values constraints)]
+       #_(println "more: " more)
+       (into #{}
+             (for [answer (keys alternatives)]
+               (let [constr (get alternatives answer)
+                     node {:feature feature-name :answer answer
+                           :alternatives constr}]
+                 (if more
+                   (assoc node :children (solve-forest more constr))
+                   node)
+                 ))))))
 
 
 (comment
 
-(solve-forest ["f1" ["yes" "yes" "no" "no"]])
+  (solve-forest [["f1" ["yes" "yes" "no" "no"]] ["f2" ["1" "2" "2" "-"]]] nil)
 
-(def f1 ["f1" ["1" "1" "2"]])
-(def values ["yes" "no" "no" "yes"])
-f1
-values
-(into #{} values)
-(get {:a 1} :b #{})
-(reduce (fn [acc itm]
-          (conj acc itm)) #{} [1 2 3])
-(reduce-indexed (fn [acc idx itm]
-                  (println (str "acc: " acc " idx: " idx " itm: " itm))
-                  (assoc acc itm (conj (get acc itm #{}) idx))) {} values)
+  (def f1 ["f1" ["1" "1" "2"]])
+  (def values ["yes" "no" "no" "yes"])
+  f1
+  values
+  (into #{} values)
+  (get {:a 1} :b #{})
+  (reduce (fn [acc itm]
+            (conj acc itm)) #{} [1 2 3])
+  (reduce-indexed (fn [acc idx itm]
+                    (println (str "acc: " acc " idx: " idx " itm: " itm))
+                    (assoc acc itm (conj (get acc itm #{}) idx))) {} values)
 
 
 
-)
+  )
--- test/fp_contest_solution_tree/core_test.clj
+++ test/fp_contest_solution_tree/core_test.clj
@@ -4,7 +4,25 @@
 
 (expect  #{{:feature "f1" :answer "1" :alternatives #{0 1}}
           {:feature "f1" :answer "2" :alternatives #{2}}}
-         (level-zero-forest ["f1" ["1" "1" "2"]]))
+         (solve-forest [["f1" ["1" "1" "2"]]]))
 
-(expect {"no" #{1 2}, "yes" #{0 3}}
+
+(expect #{{:feature "f1" :answer "1" :alternatives #{0 1}
+           :children #{{:feature "f2" :answer "yes" :alternatives #{0}}
+                       {:feature "f2" :answer "no" :alternatives #{1}}}}
+          {:feature "f1" :answer "2" :alternatives #{2}
+           :children #{{:feature "f2" :answer "no" :alternatives #{2}}}}}
+        (solve-forest [["f1" ["1" "1" "2"]]
+                       ["f2" ["yes" "no" "no"]]]))
+
+(expect {"no" #{1 2} "yes" #{0 3}}
         (alternatives ["yes" "no" "no" "yes"]))
+
+(expect {"yes" #{0 3}}
+        (alternatives ["yes" "no" "no" "yes"] #{0 3}))
+
+(expect {"yes" #{0 3} "no" #{1 2}}
+        (alternatives ["yes" "no" "no" "yes"] nil))
+
+(expect {"yes" #{0} "no" #{1 2}}
+        (alternatives ["yes" "no" "no" "-"]))
