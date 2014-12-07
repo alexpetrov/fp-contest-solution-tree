06b0aae40e52e3bc77d092c372f019339f032073
alexpetrov <alexpetrov.rb@gmail.com>
2014-12-07 13:31:47 +0300

    Refactor rename s/forest/tree/g

--- src/fp_contest_solution_tree/core.clj
+++ src/fp_contest_solution_tree/core.clj
@@ -14,25 +14,22 @@
           (assoc acc itm (conj (get acc itm #{}) idx))
           acc)) {} feature-values)))
 
-(defn solve-forest
-  ([features] (solve-forest features nil))
+(defn solve-tree
+  ([features] (solve-tree features nil))
   ([[[feature-name feature-values] & more] constraints]
      (let [alternatives (alternatives feature-values constraints)]
-       #_(println "more: " more)
        (into #{}
              (for [answer (keys alternatives)]
                (let [constr (get alternatives answer)
                      node {:feature feature-name :answer answer
                            :alternatives constr}]
                  (if more
-                   (assoc node :children (solve-forest more constr))
-                   node)
-                 ))))))
-
+                   (assoc node :children (solve-tree more constr))
+                   node)))))))
 
 (comment
 
-  (solve-forest [["f1" ["yes" "yes" "no" "no"]] ["f2" ["1" "2" "2" "-"]]] nil)
+  (solve-tree [["f1" ["yes" "yes" "no" "no"]] ["f2" ["1" "2" "2" "-"]]] nil)
 
   (def f1 ["f1" ["1" "1" "2"]])
   (def values ["yes" "no" "no" "yes"])
--- test/fp_contest_solution_tree/core_test.clj
+++ test/fp_contest_solution_tree/core_test.clj
@@ -4,7 +4,7 @@
 
 (expect  #{{:feature "f1" :answer "1" :alternatives #{0 1}}
           {:feature "f1" :answer "2" :alternatives #{2}}}
-         (solve-forest [["f1" ["1" "1" "2"]]]))
+         (solve-tree [["f1" ["1" "1" "2"]]]))
 
 
 (expect #{{:feature "f1" :answer "1" :alternatives #{0 1}
@@ -12,7 +12,7 @@
                        {:feature "f2" :answer "no" :alternatives #{1}}}}
           {:feature "f1" :answer "2" :alternatives #{2}
            :children #{{:feature "f2" :answer "no" :alternatives #{2}}}}}
-        (solve-forest [["f1" ["1" "1" "2"]]
+        (solve-tree [["f1" ["1" "1" "2"]]
                        ["f2" ["yes" "no" "no"]]]))
 
 (expect {"no" #{1 2} "yes" #{0 3}}
